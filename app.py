04047942c43005330a70f7e8c4f6849dc3fcdbeb
Dylan-Ian-Ruiz <druiz@fi.uba.ar>
2025-09-30 02:32:31 -0300

    Hago PR con la logica de enviar mail terminada (#6)
    * Agrego los cambios hechos para la logica de enviar mail(falta validacion de google)
    
    * Termino la logica de enviar mail, usando try except

--- app.py
+++ app.py
@@ -15,17 +15,23 @@ def home():
 
 if __name__ == '__main__':
     app.run("localhost", port=808)'''
-
+import os
 from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__) 
  
-app.config['MAIL_SERVER'] = 'smtp.gmail.com' 
-app.config['MAIL_PORT'] = 587 
-app.config['MAIL_USE_TLS'] = True 
-app.config['MAIL_USE_SSL'] = False 
-app.config['MAIL_USERNAME'] = 'your-email@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'your-email-password' 
-app.config['MAIL_DEFAULT_SENDER'] = 'your-email@gmail.com' 
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL') == 'True'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+
+mail = Mail(app)
+ 
  
  
 diccionario = { 
@@ -59,11 +65,35 @@ def formulario():
 @app.errorhandler(404)
 def page_not_found(e):
        mensaje="Error de página"
-       return render_template('error.html',msj=mensaje)
+       return render_template('error.html',msj=mensaje, info_evento=diccionario)
  
 @app.route('/')
 def home ():
     return render_template('index.html', info_evento=diccionario)
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    try:
+        nombre = request.form['nombre']
+        apellido= request.form['apellido_pat']
+        carrera=request.form['carrera']
+        dni= request.form['DNI']
+        nacimiento= request.form['nacimiento']
+        nacionalidad= request.form['nacionalidad']
+        observaciones= request.form['observaciones']
+    
+
+        msg = Message(
+            subject=f"Inscripcion de: {nombre}",
+            recipients=['mtbrally51@gmail.com'], 
+            body=f"Nombre: {nombre}\nApellido: {apellido}\nDNI: {dni}\nFecha de nacimiento: {nacimiento}\nTipo de carrera: {carrera}\nNacionalidad: {nacionalidad}\nMensaje:\n{observaciones}"
+        )
+
+        mail.send(msg)
+
+        return "Mensaje enviado correctamente"
+    except Exception as e:
+        print(f"Error enviando mail: {e}") 
+        flash("Hubo un error al enviar tu mensaje, intenta más tarde")
 
 if __name__== '__main__':
         app.run("localhost", port=8080, debug=True)
